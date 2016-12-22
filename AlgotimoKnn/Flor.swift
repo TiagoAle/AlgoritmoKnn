f63e9d88fdf3ea3675beeb7b98612396f9322c71
Tiago Queiroz <tiagoqueiroz@mbp-de-tiago.home>
2016-12-22 12:46:35 -0300

    Falta só os pesos

--- AlgotimoKnn/Flor.swift
+++ AlgotimoKnn/Flor.swift
@@ -7,7 +7,112 @@
 //
 
 import UIKit
+import Darwin
+
+struct FlorDistance {
+    var index: Int
+    var distance: Double
+}
 
 class Flor: NSObject {
+    private(set) var classe: String?
+    var a,b,c,d: Double?
+    override var description: String {
+        return "\(a!) | \(b!) | \(c!) | \(d!) | \(classe!)\n"
+    }
+    
+    override init() {
+        super.init()
+    }
+    
+    init(a: Double, b: Double, c: Double, d: Double, classe: String) {
+        self.a = a
+        self.b = b
+        self.c = c
+        self.d = d
+        self.classe = classe
+        
+    }
+    
+    func getA() -> Double {
+        return self.a!
+    }
+    func getB() -> Double {
+        return self.b!
+    }
+    func getC() -> Double {
+        return self.c!
+    }
+    func getD() -> Double {
+        return self.d!
+    }
+    func getClasse() -> String {
+        return self.classe!
+    }
+
+    //função para obter a distância euclidiana entre dois objetos do tipo Flor
+    static func obterDistanciaEuclidiana(flor1: Flor, flor2: Flor) -> Double{
+        let soma = pow(flor1.getA() - flor2.getA(), 2) + pow(flor1.getB() - flor2.getB(), 2) + pow(flor1.getC() - flor2.getC(), 2) + pow(flor1.getD() - flor2.getD(), 2)
+        return sqrt(soma)
+    }
+    
+    //função que classifica uma Flor de acordo com o algoritmo knn
+    static func classificarAmostra(flores: [Flor], novaFlor: Flor, k: Int) -> String {
+        var q = k
+        if q % 2 == 0 {
+            q = q - 1
+            if q <= 0 {
+                q = 1
+            }
+        }
+        
+        var dist_individuos = [FlorDistance]()
+        var aux: FlorDistance?
+        var contk = 0
+        var contVersicolor = 0
+        var contSetosa = 0
+        var contVirginica = 0
+        
+        //armazena os indices das flores de treinamento e as distâncias para a flor a ser classificada
+        for i in 0...flores.count-1{
+            let dist = obterDistanciaEuclidiana(flor1: flores[i], flor2: novaFlor)
+            
+            aux = FlorDistance(index: i, distance: dist)
+        
+            dist_individuos.append(aux!)
+            
+        }
+        
+        //ordena o array de indices/distância pela menor distância
+        dist_individuos = dist_individuos.sorted(by: { $0.distance < $1.distance })
+        
+        //verifica a classe dos k elementos mais próximos da flor a ser classificada
+        for j in dist_individuos{
+            let classe = flores[j.index].getClasse()
+            if classe == "Iris-setosa"{
+                contSetosa = contSetosa+1
+            }else if classe == "Iris-versicolor"{
+                contVersicolor = contVersicolor + 1
+            }else{
+                contVirginica = contVirginica + 1
+            }
+            
+            if contk > k{ break}
+            
+            contk = contk + 1
+        }
+        
+        var classeNovaFlor = ""
+        
+        //classifica a nova flor de acordo com a classe que apareceu mais vezes dentro dos k elementos
+        if contSetosa >= contVersicolor && contSetosa >= contVirginica {
+            classeNovaFlor = "Iris-setosa"
+        }else if contSetosa <= contVersicolor && contVersicolor >= contVirginica {
+            classeNovaFlor = "Iris-versicolor"
+        }else{
+            classeNovaFlor = "Iris-virginica"
+        }
+        return classeNovaFlor
+    }
 
 }
--- AlgotimoKnn/ViewController.swift
+++ AlgotimoKnn/ViewController.swift
@@ -7,11 +7,30 @@
 //
 
 import UIKit
+import Darwin
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var labelDesvio: UILabel!
+    @IBOutlet weak var labelMedia: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var treinamento: UITextField!
+    @IBOutlet weak var amostra: UITextField!
+    @IBOutlet weak var qtdK: UITextField!
+    var flores = [Flor]()
+    var floresAux = [Flor]()
+    var floresTreinamento = [Flor]()
+    var floresAmostra = [Flor]()
+    
+    var k = 50
+    
+    var tamTreinamento = 100
+    var tamTestes: Int?
+    var arrayAcertos = [Int]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.textView.text = ""
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +39,111 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func actionPrincipal(_ sender: Any) {
+        arrayAcertos = []
+        self.textView.text = ""
+        getTxt()
+        
+        k = Int(qtdK.text!)!
+        
+        tamTreinamento = Int(treinamento.text!)!
+        
+        tamTestes = Int(amostra.text!)!
+        
+        let media = mediaAcertos()
+        
+        let porcentagem = media/Double(tamTestes!)
+        
+        let desvio = calcularDesvio(array: arrayAcertos, media: media)
+        
+        self.labelDesvio.text = String.init(format: "%.2f", desvio)
 
+        self.labelMedia.text = String.init(format: "%.2f", porcentagem*100)
+
+    }
+    //Pega os dados do arquico iris.txt e transforma em um array de String
+    func getTxt(){
+        var text: [String]?
+        if let path = Bundle.main.path(forResource: "iris", ofType: "txt") {
+            do {
+                let data = try String(contentsOfFile: path, encoding: .utf8)
+                let myStrings = data.components(separatedBy: .newlines)
+                //text = myStrings.joined(separator: ",")
+                text = myStrings
+            } catch {
+                print(error)
+            }
+        }
+        //print(text!)
+        
+        //Transforma cada linha do arquivo em um objeto do tipo Flor
+        for str in text!{
+            if str != ""{
+                let aux = str.components(separatedBy: ",")
+                print(aux)
+                let flor = Flor(a: Double(aux[0])! , b: Double(aux[1])!, c: Double(aux[2])!, d: Double(aux[3])!, classe: aux[4])
+                flores.append(flor)
+            }
+        }
+    }
+    
+    //Embaralha o array de treinamento e o array de amostra
+    func radomTreinamentoAmostra() {
+        floresAux = flores
+        for _ in 0...tamTreinamento-1{
+            let tam = UInt32(floresAux.count)
+            let index = arc4random()%tam
+            floresTreinamento.append(floresAux[Int(index)])
+            floresAux.remove(at: Int(index))
+        }
+        for _ in 0...floresAux.count-1{
+            let tam = UInt32(floresAux.count)
+            let index = arc4random()%tam
+            floresAmostra.append(floresAux[Int(index)])
+            floresAux.remove(at: Int(index))
+        }
+    }
+    
+    
+    //Pega 10 elementos do array de amostra para serem classificados através do algoritmo knn e verifica o resultado
+    func verificaAmostra() -> Int{
+        var acertos = 0
+        for i in 0...tamTestes!-1{
+            let flor = floresAmostra[i]
+            let classeObtida = Flor.classificarAmostra(flores: floresTreinamento, novaFlor: flor, k: k)
+            print("\nClasse Obtida: \(classeObtida)")
+            print(("Classe Desejada: \(flor.classe!)"))
+            self.textView.text.append("Classe Obtida: \(classeObtida)\n")
+            self.textView.text.append("Classe Desejada: \(flor.classe!)\n")
+            if classeObtida == flor.classe!{
+                acertos = acertos + 1
+            }
+        }
+        print("Acertos: \(acertos) em 10 testes")
+        arrayAcertos.append(acertos)
+        self.textView.text.append("Acertos: \(acertos) em \(tamTestes!) testes\n\n")
+        return acertos
+    }
+    
+    // Retorna o número médio de acertos
+    func mediaAcertos() -> Double{
+        var soma = 0
+        for _ in 0...29{
+            radomTreinamentoAmostra()
+            soma = soma + verificaAmostra()
+        }
+        return Double(soma)/Double(30)
+    }
+    
+    //Retorna o desvio padrão
+    func calcularDesvio(array: [Int], media: Double) -> Double{
+        var soma = 0.0
+        for i in array{
+            soma = soma + pow((media - Double(i)), 2)
+        }
+        print(soma)
+        return sqrt(soma/Double(30*tamTestes!))
+    }
+    
 }
 
